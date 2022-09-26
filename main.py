05092079d740f83b7d2eab8b594cd7ba1e2db3bf
Alex Burneikis <alexburneikis@gmail.com>
2022-09-26 15:48:44 +1000

    Code cleanup

--- main.py
+++ main.py
@@ -73,50 +73,41 @@ def evaluate(board):
 def getBestMove(board, depth, color):
     legalMoves = generateLegalMoves(board)
 
-    bestMove = legalMoves[0]
-    
-    if depth == 0:
-        return bestMove
+    #guard statement if reach max depth
+    if depth == 0: return legalMoves[0]
 
+    #set defaults
+    bestMove = legalMoves[0]
     bestScore = 0
 
     for i in legalMoves:
         board.push_san(i)
+
         if isWin(board):
             board.pop()
             return i
-        board.push_san(getBestMove(board, depth - 1, ("White" if color == "Black" else "Black")))
+
+        #look deeper
+        board.push_san(getBestMove(board, depth - 1, not color))
         score = evaluate(board)
+
+        #go back to previous state
         board.pop()
         board.pop()
 
-        if score >= bestScore and color == "White":
-            bestScore = score
-            bestMove = i
-
-        if score <= bestScore and color == "Black":
-            bestScore = score
-            bestMove = i
+        #see if this move is better than the previous best move
+        if color:
+            if score > bestScore:
+                bestScore = score
+                bestMove = i
+        else:
+            if score < bestScore:
+                bestScore = score
+                bestMove = i
 
     return bestMove
 
-def getAiMove(board):
-    legalMoves = generateLegalMoves(board)
-
-    player = "White" if board.turn else "Black"
-
-    #if player can win, win
-    for i in legalMoves:
-        board.push_san(i)
-        if isWin(board) == player:
-            board.pop()
-            return i
-        board.pop()
-
-    return getBestMove(board, 8, player)
-
 def game():
-    # make decapawn game
     board = chess.Board()
 
     board.set_board_fen("8/8/8/ppppp3/8/8/8/PPPPP3")
@@ -125,16 +116,17 @@ def game():
 
     while not isWin(board):
         print(board)
-        board.push_san(getAiMove(board))
+
+        board.push_san(getBestMove(board, 6, board.turn))
 
         moves.append(str(board.peek()))
 
+    #post-game
     print(board)
     if isWin(board) == "White":
         print("White wins")
     elif isWin(board) == "Black":
         print("Black wins")
-
     print(moves)
 
 game()
